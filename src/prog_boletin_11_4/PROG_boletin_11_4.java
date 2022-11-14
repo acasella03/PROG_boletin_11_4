22af46557663a9410b709119fefb5e08b81053c1
Angi Casella <acasella@danielcastelao.org>
2022-11-14 20:40:53 +0100

    Correción del código

--- src/prog_boletin_11_4/PROG_boletin_11_4.java
+++ src/prog_boletin_11_4/PROG_boletin_11_4.java
@@ -10,15 +10,13 @@ public class PROG_boletin_11_4 {
         System.out.println("Teclea número");
         int num = scaner.nextInt();
 
-        if (num != 0) {
+        while (num != 0) {
             System.out.println("Tabla de multiplicar:");
             for (int i = 0; i < 11; i++) {
                 System.out.println(num + " x " + i + " = " + num * i);
             }
-
-        } else {
-            System.out.println("Fin");
+            System.out.println("Teclea número");
+            num = scaner.nextInt();
         }
     }
-
 }
